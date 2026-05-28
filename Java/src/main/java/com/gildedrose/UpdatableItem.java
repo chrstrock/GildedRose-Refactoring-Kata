cafd55f8a4c54b93a13a9508c2d2772c5793a5d8
David Ferrell <chrstrock@gmail.com>
2026-05-28 16:34:41 -0400

    converted aged brie check to boolean.

---
+++ Java/src/main/java/com/gildedrose/UpdatableItem.java
@@ -0,0 +1,61 @@
+package com.gildedrose;
+
+public class UpdatableItem extends Item{
+
+    public UpdatableItem(String name, int sellIn, int quality){
+        super(name, sellIn, quality);
+    }
+
+    static void updateItemQuality(Item item) {
+        boolean isAgedBrie = item.name.equals("Aged Brie");
+        boolean isBackStagePass = item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+        if (!isAgedBrie
+            && !isBackStagePass) {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+
+                if (isBackStagePass) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+            if (!isAgedBrie) {
+                if (!isBackStagePass) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+    }
+}
